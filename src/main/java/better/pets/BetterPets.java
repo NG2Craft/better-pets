e05b40e1407f39e506da5676386b9e3b75dd7461
Elisicc <elias.breitler@gmail.com>
2025-07-26 19:18:54 +0200

    ✨ FEAT |  Detect damage source from entity

--- src/main/java/better/pets/BetterPets.java
+++ src/main/java/better/pets/BetterPets.java
@@ -1,24 +1,37 @@
 package better.pets;
 
-import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+
 public class BetterPets implements ModInitializer {
 	public static final String MOD_ID = "better-pets";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		LOGGER.info("[BetterPets] Mod geladen, test");
 
-		LOGGER.info("Hello Fabric world!");
+		ServerLivingEntityEvents.ALLOW_DAMAGE.register((LivingEntity entity, DamageSource source, float amount) -> {
+			LOGGER.info("[BetterPets] Schaden erkannt an: " + entity.getName().getString());
+			
+			Entity attacker = source.getAttacker();
+			if (attacker != null) {
+				LOGGER.info("Attacker: " + attacker.getName().getString());
+			} else {
+				LOGGER.info("Attacker: nicht erkannt");
+			}
+			
+			return true; 
+		});
+
 	}
 }
